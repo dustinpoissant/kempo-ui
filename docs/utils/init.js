3d12e4bbaa55ecb5e8247fea81582830ad4ee100
dusitnpoissant <dustinpoissant@gmail.com>
2025-10-13 16:16:48 -0400

    litDisableBundleWarning

--- docs/utils/init.js
+++ docs/utils/init.js
@@ -5,3 +5,4 @@ import ShadowComponent from '../src/components/ShadowComponent.js';
 Import.replacements.root = '../';
 Icon.pathToIcons = ['../icons'];
 ShadowComponent.stylesheetPath = '../kempo.min.css';
+
