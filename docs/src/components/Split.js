384a5a96bd0e10aa9c3ba953ba8af339fee3791a
Dustin Poissant <dustinpoissant@gmail.com>
2026-04-22 16:17:01 -0400

    Added grip to Split component

--- docs/src/components/Split.js
+++ docs/src/components/Split.js
@@ -1,7 +1,19 @@
-import{html as e,css as t}from"../lit-all.min.js";import i from"./ShadowComponent.js";import s from"../utils/drag.js";export default class r extends i{static properties={resizing:{type:Boolean,reflect:!0},stacked:{type:Boolean,reflect:!0},stackWidth:{type:Number,attribute:"stack-width"},direction:{type:String,reflect:!0},persistentId:{type:String,reflect:!0,attribute:"persistent-id"}};constructor(){super(),this.resizing=!1,this.stacked=!1,this.stackWidth=0,this.direction="horizontal",this.persistentId=null,this.dragStartSize=0,this.dragCleanup=()=>{},this.resizeObserver=null}firstUpdated(){if(super.firstUpdated(),this.setupDragHandler(),this.setupResizeObserver(),this.persistentId&&window?.localStorage){const e=window.localStorage.getItem(`split-persistent-id-${this.persistentId}`);e&&this.setSize(e)}}disconnectedCallback(){super.disconnectedCallback(),this.dragCleanup(),this.resizeObserver&&(this.resizeObserver.disconnect(),this.resizeObserver=null)}handleDragStart=()=>{this.resizing=!0,this.dragStartSize=Math.round(this.shadowRoot.getElementById("pane-1").getBoundingClientRect()["vertical"===this.direction?"height":"width"]),this.dispatchEvent(new CustomEvent("resizestart",{detail:{startSize:this.dragStartSize},bubbles:!0}))};handleDrag=({x:e,y:t})=>{const i="vertical"===this.direction?t:e,s=`${this.dragStartSize+i}px`;this.setSize(s),this.dispatchEvent(new CustomEvent("resize",{detail:{size:s},bubbles:!0}))};handleDragEnd=({x:e,y:t})=>{this.resizing=!1;const i="vertical"===this.direction?t:e,s=`${this.dragStartSize+i}px`;this.setSize(s),this.persistentId&&window?.localStorage&&window.localStorage.setItem(`split-persistent-id-${this.persistentId}`,s),this.dispatchEvent(new CustomEvent("resizeend",{detail:{size:s},bubbles:!0}))};setSize(e){this.style.setProperty("--pane_1_size",e)}setupDragHandler(){const e=this.shadowRoot.getElementById("divider-handle");e&&(this.dragCleanup=s({element:e,callback:this.handleDrag,startCallback:this.handleDragStart,endCallback:this.handleDragEnd}))}setupResizeObserver(){this.resizeObserver=new ResizeObserver(e=>{for(const t of e){const e=t.contentRect.width;this.stacked=e<=this.stackWidth}}),this.resizeObserver.observe(this)}static styles=t`
+import{html as e,css as t}from"../lit-all.min.js";import i from"./ShadowComponent.js";import s from"../utils/drag.js";import{boolExists as r}from"../utils/propConverters.js";export default class d extends i{static properties={resizing:{type:Boolean,reflect:!0},stacked:{type:Boolean,reflect:!0},stackWidth:{type:Number,attribute:"stack-width"},direction:{type:String,reflect:!0},persistentId:{type:String,reflect:!0,attribute:"persistent-id"},grip:{type:Boolean,reflect:!0,converter:r}};constructor(){super(),this.resizing=!1,this.stacked=!1,this.stackWidth=0,this.direction="horizontal",this.persistentId=null,this.grip=!1,this.dragStartSize=0,this.dragCleanup=()=>{},this.resizeObserver=null}firstUpdated(){if(super.firstUpdated(),this.grip){const e=document.createElement("div");e.style.cssText="position:absolute;visibility:hidden;width:var(--handle_width)",this.shadowRoot.appendChild(e);const t=e.offsetWidth;this.shadowRoot.removeChild(e);const i=t%2==0?t-1:t;this.style.setProperty("--handle_grip_width",`${i}px`)}if(this.setupDragHandler(),this.setupResizeObserver(),this.persistentId&&window?.localStorage){const e=window.localStorage.getItem(`split-persistent-id-${this.persistentId}`);e&&this.setSize(e)}}disconnectedCallback(){super.disconnectedCallback(),this.dragCleanup(),this.resizeObserver&&(this.resizeObserver.disconnect(),this.resizeObserver=null)}handleDragStart=()=>{this.resizing=!0,this.dragStartSize=Math.round(this.shadowRoot.getElementById("pane-1").getBoundingClientRect()["vertical"===this.direction?"height":"width"]),this.dispatchEvent(new CustomEvent("resizestart",{detail:{startSize:this.dragStartSize},bubbles:!0}))};handleDrag=({x:e,y:t})=>{const i="vertical"===this.direction?t:e,s=`${this.dragStartSize+i}px`;this.setSize(s),this.dispatchEvent(new CustomEvent("resize",{detail:{size:s},bubbles:!0}))};handleDragEnd=({x:e,y:t})=>{this.resizing=!1;const i="vertical"===this.direction?t:e,s=`${this.dragStartSize+i}px`;this.setSize(s),this.persistentId&&window?.localStorage&&window.localStorage.setItem(`split-persistent-id-${this.persistentId}`,s),this.dispatchEvent(new CustomEvent("resizeend",{detail:{size:s},bubbles:!0}))};setSize(e){this.style.setProperty("--pane_1_size",e)}setupDragHandler(){const e=this.shadowRoot.getElementById("divider-handle");e&&(this.dragCleanup=s({element:e,callback:this.handleDrag,startCallback:this.handleDragStart,endCallback:this.handleDragEnd}))}setupResizeObserver(){this.resizeObserver=new ResizeObserver(e=>{for(const t of e){const e=t.contentRect.width;this.stacked=e<=this.stackWidth}}),this.resizeObserver.observe(this)}render(){return e`
+			<div id="pane-1" class="pane">
+				<slot></slot>
+			</div>
+			<div id="divider-handle">
+				<div id="divider-border">
+          ${this.grip?e`<div id="divider-grip"></div>`:""}
+        </div>
+			</div>
+			<div id="pane-2" class="pane">
+				<slot name="right"></slot>
+			</div>
+		`}static styles=t`
 		:host {
 			--pane_1_size: calc((100% - var(--handle_width)) / 2);
-			--handle_width: 0.5rem;
+			--handle_width: 7px;
 			--min_pane_size: 6rem;
 
 			height: 100%;
@@ -27,6 +39,7 @@ import{html as e,css as t}from"../lit-all.min.js";import i from"./ShadowComponen
 		}
 
 		#divider-handle {
+			position: relative;
 			display: flex;
 			flex-shrink: 0;
 			justify-content: center;
@@ -47,9 +60,20 @@ import{html as e,css as t}from"../lit-all.min.js";import i from"./ShadowComponen
 		}
 
 		#divider-border {
+			position: relative;
 			width: 1px;
 			height: 100%;
 			border-left: 1px solid var(--c_border);
+		}
+		#divider-grip {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			width: var(--handle_grip_width, var(--handle_width));
+			height: 2rem;
+			border-radius: 0.25rem;
+			background-color: var(--c_border);
 		}
 
 		#pane-2 {
@@ -105,14 +129,8 @@ import{html as e,css as t}from"../lit-all.min.js";import i from"./ShadowComponen
 			border-left: none;
 			border-top: 1px solid var(--c_border);
 		}
-	`;render(){return e`
-			<div id="pane-1" class="pane">
-				<slot></slot>
-			</div>
-			<div id="divider-handle">
-				<div id="divider-border"></div>
-			</div>
-			<div id="pane-2" class="pane">
-				<slot name="right"></slot>
-			</div>
-		`}}customElements.define("k-split",r);
+		:host([direction="vertical"]) #divider-grip {
+			width: 2rem;
+			height: 0.5rem;
+		}
+	`}customElements.define("k-split",d);
--- src/components/Split.js
+++ src/components/Split.js
@@ -1,125 +1,157 @@
 import { html, css } from '../lit-all.min.js';
 import ShadowComponent from './ShadowComponent.js';
 import drag from '../utils/drag.js';
+import { boolExists } from '../utils/propConverters.js';
 
 export default class Split extends ShadowComponent {
-	static properties = {
-		resizing: { type: Boolean, reflect: true },
-		stacked: { type: Boolean, reflect: true },
-		stackWidth: { type: Number, attribute: 'stack-width' },
-		direction: { type: String, reflect: true },
-		persistentId: { type: String, reflect: true, attribute: 'persistent-id' }
-	};
+  static properties = {
+    resizing: { type: Boolean, reflect: true },
+    stacked: { type: Boolean, reflect: true },
+    stackWidth: { type: Number, attribute: 'stack-width' },
+    direction: { type: String, reflect: true },
+    persistentId: { type: String, reflect: true, attribute: 'persistent-id' },
+    grip: { type: Boolean, reflect: true, converter: boolExists }
+  };
 
-	constructor() {
-		super();
-		
-		this.resizing = false;
-		this.stacked = false;
-		this.stackWidth = 0;
-		this.direction = 'horizontal';
-		this.persistentId = null;
-		
-		// Private state
-		this.dragStartSize = 0;
-		this.dragCleanup = () => {};
-		this.resizeObserver = null;
-	}
+  constructor() {
+    super();
 
-	/*
-		Lifecycle Callbacks
-	*/
-	firstUpdated() {
-		super.firstUpdated();
-		this.setupDragHandler();
-		this.setupResizeObserver();
-		if(this.persistentId && window?.localStorage) {
-			const size = window.localStorage.getItem(`split-persistent-id-${this.persistentId}`);
-			if(size) this.setSize(size);
-		}
-	}
+    this.resizing = false;
+    this.stacked = false;
+    this.stackWidth = 0;
+    this.direction = 'horizontal';
+    this.persistentId = null;
+    this.grip = false;
 
-	disconnectedCallback() {
-		super.disconnectedCallback();
-		this.dragCleanup();
-		if(this.resizeObserver) {
-			this.resizeObserver.disconnect();
-			this.resizeObserver = null;
-		}
-	}
+    // Private state
+    this.dragStartSize = 0;
+    this.dragCleanup = () => { };
+    this.resizeObserver = null;
+  }
 
-	/*
-		Event Handlers
-	*/
-	handleDragStart = () => {
-		this.resizing = true;
-		this.dragStartSize = Math.round(this.shadowRoot.getElementById('pane-1').getBoundingClientRect()[this.direction === 'vertical' ? 'height' : 'width']);
-		this.dispatchEvent(new CustomEvent('resizestart', {
-			detail: { startSize: this.dragStartSize },
-			bubbles: true
-		}));
-	};
+  /*
+    Lifecycle Callbacks
+  */
+  firstUpdated() {
+    super.firstUpdated();
+    if(this.grip){
+      const el = document.createElement('div');
+      el.style.cssText = 'position:absolute;visibility:hidden;width:var(--handle_width)';
+      this.shadowRoot.appendChild(el);
+      const handleWidth = el.offsetWidth;
+      this.shadowRoot.removeChild(el);
+      const gripWidth = handleWidth % 2 === 0 ? handleWidth - 1 : handleWidth;
+      this.style.setProperty('--handle_grip_width', `${gripWidth}px`);
+    }
+    this.setupDragHandler();
+    this.setupResizeObserver();
+    if (this.persistentId && window?.localStorage) {
+      const size = window.localStorage.getItem(`split-persistent-id-${this.persistentId}`);
+      if (size) this.setSize(size);
+    }
+  }
 
-	handleDrag = ({ x, y }) => {
-		const delta = this.direction === 'vertical' ? y : x;
-		const size = `${this.dragStartSize + delta}px`;
-		this.setSize(size);
-		this.dispatchEvent(new CustomEvent('resize', {
-			detail: { size },
-			bubbles: true
-		}));
-	};
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.dragCleanup();
+    if (this.resizeObserver) {
+      this.resizeObserver.disconnect();
+      this.resizeObserver = null;
+    }
+  }
 
-	handleDragEnd = ({ x, y }) => {
-		this.resizing = false;
-		const delta = this.direction === 'vertical' ? y : x;
-		const size = `${this.dragStartSize + delta}px`;
-		this.setSize(size);
-		if(this.persistentId && window?.localStorage) {
-			window.localStorage.setItem(`split-persistent-id-${this.persistentId}`, size);
-		}
-		this.dispatchEvent(new CustomEvent('resizeend', {
-			detail: { size },
-			bubbles: true
-		}));
-	};
+  /*
+    Event Handlers
+  */
+  handleDragStart = () => {
+    this.resizing = true;
+    this.dragStartSize = Math.round(this.shadowRoot.getElementById('pane-1').getBoundingClientRect()[this.direction === 'vertical' ? 'height' : 'width']);
+    this.dispatchEvent(new CustomEvent('resizestart', {
+      detail: { startSize: this.dragStartSize },
+      bubbles: true
+    }));
+  };
 
-	/*
-		Public Methods
-	*/
-	setSize(size) {
-		this.style.setProperty('--pane_1_size', size);
-	}
+  handleDrag = ({ x, y }) => {
+    const delta = this.direction === 'vertical' ? y : x;
+    const size = `${this.dragStartSize + delta}px`;
+    this.setSize(size);
+    this.dispatchEvent(new CustomEvent('resize', {
+      detail: { size },
+      bubbles: true
+    }));
+  };
 
-	setupDragHandler() {
-		const handle = this.shadowRoot.getElementById('divider-handle');
-		if(handle) {
-			this.dragCleanup = drag({
-				element: handle,
-				callback: this.handleDrag,
-				startCallback: this.handleDragStart,
-				endCallback: this.handleDragEnd
-			});
-		}
-	}
+  handleDragEnd = ({ x, y }) => {
+    this.resizing = false;
+    const delta = this.direction === 'vertical' ? y : x;
+    const size = `${this.dragStartSize + delta}px`;
+    this.setSize(size);
+    if (this.persistentId && window?.localStorage) {
+      window.localStorage.setItem(`split-persistent-id-${this.persistentId}`, size);
+    }
+    this.dispatchEvent(new CustomEvent('resizeend', {
+      detail: { size },
+      bubbles: true
+    }));
+  };
 
-	setupResizeObserver() {
-		this.resizeObserver = new ResizeObserver(entries => {
-			for(const entry of entries) {
-				const width = entry.contentRect.width;
-				this.stacked = width <= this.stackWidth;
-			}
-		});
-		this.resizeObserver.observe(this);
-	}
+  /*
+    Public Methods
+  */
+  setSize(size) {
+    this.style.setProperty('--pane_1_size', size);
+  }
 
-	/*
-		Styles
-	*/
-	static styles = css`
+  setupDragHandler() {
+    const handle = this.shadowRoot.getElementById('divider-handle');
+    if (handle) {
+      this.dragCleanup = drag({
+        element: handle,
+        callback: this.handleDrag,
+        startCallback: this.handleDragStart,
+        endCallback: this.handleDragEnd
+      });
+    }
+  }
+
+  setupResizeObserver() {
+    this.resizeObserver = new ResizeObserver(entries => {
+      for (const entry of entries) {
+        const width = entry.contentRect.width;
+        this.stacked = width <= this.stackWidth;
+      }
+    });
+    this.resizeObserver.observe(this);
+  }
+
+  /*
+    Styles
+  */
+
+
+  /*
+    Rendering
+  */
+  render() {
+    return html`
+			<div id="pane-1" class="pane">
+				<slot></slot>
+			</div>
+			<div id="divider-handle">
+				<div id="divider-border">
+          ${this.grip ? html`<div id="divider-grip"></div>` : ''}
+        </div>
+			</div>
+			<div id="pane-2" class="pane">
+				<slot name="right"></slot>
+			</div>
+		`;
+  }
+  static styles = css`
 		:host {
 			--pane_1_size: calc((100% - var(--handle_width)) / 2);
-			--handle_width: 0.5rem;
+			--handle_width: 7px;
 			--min_pane_size: 6rem;
 
 			height: 100%;
@@ -145,6 +177,7 @@ export default class Split extends ShadowComponent {
 		}
 
 		#divider-handle {
+			position: relative;
 			display: flex;
 			flex-shrink: 0;
 			justify-content: center;
@@ -165,9 +198,20 @@ export default class Split extends ShadowComponent {
 		}
 
 		#divider-border {
+			position: relative;
 			width: 1px;
 			height: 100%;
 			border-left: 1px solid var(--c_border);
+		}
+		#divider-grip {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			width: var(--handle_grip_width, var(--handle_width));
+			height: 2rem;
+			border-radius: 0.25rem;
+			background-color: var(--c_border);
 		}
 
 		#pane-2 {
@@ -223,24 +267,11 @@ export default class Split extends ShadowComponent {
 			border-left: none;
 			border-top: 1px solid var(--c_border);
 		}
+		:host([direction="vertical"]) #divider-grip {
+			width: 2rem;
+			height: 0.5rem;
+		}
 	`;
-
-	/*
-		Rendering
-	*/
-	render() {
-		return html`
-			<div id="pane-1" class="pane">
-				<slot></slot>
-			</div>
-			<div id="divider-handle">
-				<div id="divider-border"></div>
-			</div>
-			<div id="pane-2" class="pane">
-				<slot name="right"></slot>
-			</div>
-		`;
-	}
 }
 
 customElements.define('k-split', Split);
