3f666977e6d51b743f36e56d97d074265d5b9c76
dusitnpoissant <dustinpoissant@gmail.com>
2026-05-03 16:58:42 -0400

    task/0004: Build artifacts

--- docs/src/components/SegmentedControl.js
+++ docs/src/components/SegmentedControl.js
@@ -1,10 +1,10 @@
-import{html as t,css as e,unsafeHTML as s}from"../lit-all.min.js";import n from"./ShadowComponent.js";class a extends n{static properties={value:{type:String,reflect:!0}};constructor(){super(),this.value=""}connectedCallback(){super.connectedCallback(),this.addEventListener("slotchange",this.handleSlotChange,!0)}disconnectedCallback(){super.disconnectedCallback(),this.removeEventListener("slotchange",this.handleSlotChange,!0)}updated(t){super.updated(t),t.has("value")&&this.dispatchEvent(new CustomEvent("change",{detail:{value:this.value},bubbles:!0}))}handleSlotChange=()=>{this.requestUpdate()};handleButtonClick=t=>{this.value=t};getOptions=()=>Array.from(this.children).filter(t=>"K-SC-OPTION"===t.tagName);render(){const e=this.getOptions();return t`
+import{html as t,css as e,unsafeHTML as s}from"../lit-all.min.js";import n from"./ShadowComponent.js";class i extends n{static formAssociated=!0;static properties={value:{type:String,reflect:!0},name:{type:String}};constructor(){if(super(),this.value="",this.name="",this.initialValue="","function"==typeof this.attachInternals)try{this.internals=this.attachInternals()}catch(t){}}connectedCallback(){super.connectedCallback(),this.initialValue=this.getAttribute("value")||"",this.addEventListener("slotchange",this.handleSlotChange,!0);const t=this.closest("form");t&&t.addEventListener("reset",this.handleReset,!0)}disconnectedCallback(){super.disconnectedCallback(),this.removeEventListener("slotchange",this.handleSlotChange,!0);const t=this.closest("form");t&&t.removeEventListener("reset",this.handleReset,!0)}updated(t){if(super.updated(t),t.has("value")){if(this.internals&&"function"==typeof this.internals.setFormValue)try{this.internals.setFormValue(this.value||null)}catch(t){}this.dispatchEvent(new CustomEvent("change",{detail:{value:this.value},bubbles:!0}))}}handleSlotChange=()=>{this.requestUpdate()};handleReset=()=>{this.value=this.initialValue};handleButtonClick=t=>{this.value=t};getOptions=()=>Array.from(this.children).filter(t=>"K-SC-OPTION"===t.tagName);render(){const e=this.getOptions();return t`
       <div class="btn-grp">
-        ${e.map(e=>{const n=e.getAttribute("value")||"",a=n===this.value;return t`
+        ${e.map(e=>{const n=e.getAttribute("value")||"",i=n===this.value;return t`
             <button
-              class="${a?"primary":""}"
+              class="${i?"primary":""}"
               @click=${()=>this.handleButtonClick(n)}
-              ?aria-pressed=${a}
+              ?aria-pressed=${i}
             >${s(e.innerHTML)}</button>
           `})}
       </div>
@@ -13,4 +13,4 @@ import{html as t,css as e,unsafeHTML as s}from"../lit-all.min.js";import n from"
     :host {
       display: block;
     }
-  `}class l extends HTMLElement{connectedCallback(){this.hasAttribute("value")||this.setAttribute("value","")}}export default a;customElements.define("k-segmented-control",a),customElements.define("k-sc-option",l);
+  `}class a extends HTMLElement{connectedCallback(){this.hasAttribute("value")||this.setAttribute("value","")}}export default i;customElements.define("k-segmented-control",i),customElements.define("k-sc-option",a);
