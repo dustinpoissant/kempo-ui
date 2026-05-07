d70baad22edca75db6d12ebb226c1d0829b82653
Dustin Poissant <dustinpoissant@gmail.com>
2026-05-07 19:48:55 -0400

    Build

--- docs/src/components/codeEditorControls/CodeEditorControl.js
+++ docs/src/components/codeEditorControls/CodeEditorControl.js
@@ -1,84 +1,4 @@
-import ShadowComponent from '../ShadowComponent.js';
-import { css } from '../../lit-all.min.js';
-
-export default class CodeEditorControl extends ShadowComponent {
-	static properties = {
-		hidden: { type: Boolean, reflect: true }
-	};
-
-	/*
-		Constructor
-	*/
-	constructor() {
-		super();
-		this.hidden = false;
-	}
-
-	/*
-		Lifecycle Callbacks
-	*/
-	connectedCallback() {
-		super.connectedCallback();
-		this.updateModeVisibility();
-		const editor = this.editor;
-		if(editor?.tagName === 'K-HTML-EDITOR'){
-			this.modeHandler = () => this.updateModeVisibility();
-			editor.addEventListener('mode-changed', this.modeHandler);
-		}
-	}
-
-	disconnectedCallback() {
-		super.disconnectedCallback();
-		if(this.modeHandler){
-			this.editor?.removeEventListener('mode-changed', this.modeHandler);
-			this.modeHandler = null;
-		}
-	}
-
-	updated(changed) {
-		super.updated(changed);
-		if(changed.has('hidden')){
-			this.dispatchEvent(new CustomEvent('control_visibility_change', { bubbles: true }));
-		}
-	}
-
-	/*
-		Mode Visibility
-	*/
-	updateModeVisibility() {
-		const editor = this.editor;
-		if(editor?.tagName === 'K-HTML-EDITOR'){
-			const shouldHide = editor.mode !== 'code';
-			if(this.hidden !== shouldHide){
-				this.hidden = shouldHide;
-				this.dispatchEvent(new CustomEvent('control_visibility_change', { bubbles: true }));
-			}
-		}
-	}
-
-	/*
-		Getters
-	*/
-	get editor() {
-		const isEditor = el => el?.tagName === 'K-CODE-EDITOR' || el?.tagName === 'K-HTML-EDITOR';
-		let current = this.getRootNode();
-		while(current instanceof ShadowRoot){
-			const host = current.host;
-			if(isEditor(host)) return host;
-			current = host.getRootNode();
-		}
-		let el = this.parentElement;
-		while(el){
-			if(isEditor(el)) return el;
-			el = el.parentElement;
-		}
-		return null;
-	}
-
-	/*
-		Styles
-	*/
-	static styles = css`
+import e from"../ShadowComponent.js";import{css as t}from"../../lit-all.min.js";export default class i extends e{static properties={hidden:{type:Boolean,reflect:!0}};constructor(){super(),this.hidden=!1}connectedCallback(){super.connectedCallback(),this.updateModeVisibility();const e=this.editor;"K-HTML-EDITOR"===e?.tagName&&(this.modeHandler=()=>this.updateModeVisibility(),e.addEventListener("mode-changed",this.modeHandler))}disconnectedCallback(){super.disconnectedCallback(),this.modeHandler&&(this.editor?.removeEventListener("mode-changed",this.modeHandler),this.modeHandler=null)}updated(e){super.updated(e),e.has("hidden")&&this.dispatchEvent(new CustomEvent("control_visibility_change",{bubbles:!0}))}updateModeVisibility(){const e=this.editor;if("K-HTML-EDITOR"===e?.tagName){const t="code"!==e.mode;this.hidden!==t&&(this.hidden=t,this.dispatchEvent(new CustomEvent("control_visibility_change",{bubbles:!0})))}}get editor(){const e=e=>"K-CODE-EDITOR"===e?.tagName||"K-HTML-EDITOR"===e?.tagName;let t=this.getRootNode();for(;t instanceof ShadowRoot;){const i=t.host;if(e(i))return i;t=i.getRootNode()}let i=this.parentElement;for(;i;){if(e(i))return i;i=i.parentElement}return null}static styles=t`
 		:host {
 			display: inline-flex;
 			align-items: center;
@@ -86,5 +6,4 @@ export default class CodeEditorControl extends ShadowComponent {
 		:host([hidden]) {
 			display: none !important;
 		}
-	`;
-}
+	`}
